0016d914e070dd98ef6a03b49359814084eae632
zebse <zebserra2121@gmail.com>
2023-06-15 11:35:09 +0300

    devices screen adjustment and scroll top done

--- script/script.js
+++ script/script.js
@@ -7,3 +7,39 @@ window.addEventListener('load', () => {
     }, 2000);
 });
 
+/*toogle menu icon*/
+const menuItems = document.querySelectorAll('.navbar-nav .nav-link');
+const navbarCollapse = document.querySelector('.navbar-collapse');
+
+menuItems.forEach(function(menuItem) {
+  menuItem.addEventListener('click', function() {
+    if (window.innerWidth < 992) {
+      navbarCollapse.classList.remove('show');
+    }
+  });
+});
+
+/*Back to top Button */
+window.addEventListener('scroll', () => {
+    let backToTopBtn = document.querySelector('#backToTopBtn');
+    if (window.pageYOffset > 500) {
+      backToTopBtn.style.display = 'block';
+    } else {
+      backToTopBtn.style.display = 'none';
+    }
+  });
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    let backToTopBtn = document.querySelector('#backToTopBtn');
+    backToTopBtn.style.display = 'none';
+  });
+  
+  document.querySelector('#backToTopBtn').addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+  
+  
+
